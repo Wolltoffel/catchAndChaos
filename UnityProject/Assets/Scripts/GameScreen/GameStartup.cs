eea51ecac146e758c4c17a41a97b5ed7dffd6bc9
Toffeefee8 <gibbon-preamp0x@icloud.com>
2023-08-08 23:44:56 +0200

    Added GameStartup Script

---
+++ UnityProject/Assets/Scripts/GameScreen/GameStartup.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStartup : MonoBehaviour
+{
+    void Start()
+    {
+        //Initiate UImanager
+        //Initiate Playstate
+        //
+    }
+}
