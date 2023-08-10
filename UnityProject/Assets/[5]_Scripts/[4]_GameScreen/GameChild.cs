41a12574fa6c7595a30fd114bb9e1038f55095ec
Wolltoffel <lzandianpour@stud.macromedia.de>
2023-08-10 20:57:55 +0200

    Finished Child State Machine

--- UnityProject/Assets/[5]_Scripts/[4]_GameScreen/GameChild.cs
+++ UnityProject/Assets/[5]_Scripts/[4]_GameScreen/GameChild.cs
@@ -19,6 +19,34 @@ abstract class ChildState : State
         gameObject = GameData.GetData<ChildData>("Child").gameObject;
     }
 
+    public ChildState Slide()
+    {
+        //Slide
+        if (InteractableInRange("Lolly", out GameObject interactableObject) )
+        {   
+            //Show Buttonprompt
+            if (Input.GetButtonDown(inputDevice+"B"))
+            {
+                //Lollyspeed
+                return new Slide();
+            }    
+        }
+        return null;
+    }
+
+    public void LollyPickUp()
+    {
+         //Lolly PickUp
+        if (InteractableInRange("Lolly", out GameObject interactableObject) )
+        {   
+            //Show Buttonprompt
+            if (Input.GetButtonDown(inputDevice+"B"))
+            {
+                //Lollyspeed
+            };    
+        }
+    }
+
     
 }
 
@@ -26,36 +54,79 @@ class Idle: ChildState
 {
     public override ChildState UpdateState()
     {
-        if (Input.GetButtonDown(inputDevice+"A"))
-        {
-
-        }
-
+        //Go to Run
         float horizontal = Input.GetAxis(inputDevice+" Horizontal");
         float vertical = Input.GetAxis(inputDevice+" Vertical");
-
         if (horizontal>0 || vertical>0)
         {
             return new Run();
         }
 
-        else if (InteractableInRange("Lolly", out GameObject interactableObject) 
-        && Input.GetButtonDown(inputDevice+"B"))
-        {
-            return new Lolly();
+        LollyPickUp();
+
+        //Slide
+        ChildState slide = Slide();
+        if (slide!=null)
+            return slide;
+
+        //Destroy Object
+        if (InteractableInRange("Slidable", out GameObject interactableObject) )
+        {   
+            //Show Buttonprompt
+            if (Input.GetButtonDown(inputDevice+"X"))
+                return new Destroy();
         }
+
+        return this;
+        
+    }
+}
+
+class Run: ChildState
+{
+    public override ChildState UpdateState()
+    {
+        LollyPickUp();
+
+        //Turn to idle
+        float horizontal = Input.GetAxis(inputDevice+" Horizontal");
+        float vertical = Input.GetAxis(inputDevice+" Vertical");
+        if (horizontal<=0 || vertical<=0)
+        {
+            return new Run();
+        }
+        
+        //Slide
+        ChildState slide = Slide();
+        if (slide!=null)
+            return slide;
 
         return this;
 
     }
 }
 
-class Run: ChildState
+class Slide: ChildState
 {
-    public override ChildState UpdateState(){return this;}
+    public override ChildState UpdateState()
+    {
+        bool slideOver= false; //ask from movement whether slide is over
+        if (slideOver)
+            return new Idle();
+        
+        return this;
+    }
 }
 
-class Lolly: ChildState
+class Destroy: ChildState
 {
-    public override ChildState UpdateState(){return this;}
+    public override ChildState UpdateState()
+    {
+        bool destroyOver= false; //ask from movement whether slide is over
+        if (destroyOver)
+            return new Idle();
+        
+        return this;
+    }
 }
+
