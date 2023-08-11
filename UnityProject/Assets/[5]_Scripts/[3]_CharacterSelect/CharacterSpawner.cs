dc5663b7242bb67009a3b9d4c232bf9b5ed4b81e
Toffeefee8 <gibbon-preamp0x@icloud.com>
2023-08-11 04:28:17 +0200

    GamePlay

---
+++ UnityProject/Assets/[5]_Scripts/[3]_CharacterSelect/CharacterSpawner.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSpawner : MonoBehaviour
+{   
+    [SerializeField] public Character character;
+    [SerializeField] public GameObject placeholder;
+    Transform parent;
+    Material[] materials;
+    string searchkey;
+
+    public void Start()
+    {
+        parent = placeholder.transform.parent;
+
+        SetSearchKey();
+
+        SpawnCharacter();
+    }
+
+    private void ApplyMaterials()
+    {    
+        ApplyGender();
+        materials = GameData.GetData<PlayerData>(searchkey).characterAssets.GetActiveMaterials();
+        placeholder.GetComponentInChildren<SkinnedMeshRenderer>().materials = materials;
+    }
+
+    private void ApplyGender()
+    {
+        Vector3 position = placeholder.transform.position;
+        Destroy(placeholder);
+        GameObject spawnableObject = GameData.GetData<PlayerData>(searchkey).characterAssets.GetContainer().prefab;
+        placeholder = Instantiate (spawnableObject, position, Quaternion.identity);
+        placeholder.transform.parent = parent;
+        placeholder.name = searchkey + "_Spawned";
+    }
+
+    public void SetData()
+    {
+        var temp = GameData.GetData<PlayerData>(searchkey);
+        temp.gameObject = placeholder;
+        Debug.Log(temp.gameObject);
+    }
+
+    public void SpawnCharacter()
+    {
+        ApplyGender();
+        ApplyMaterials();
+        SetData();
+    }
+
+    public void SetSearchKey()
+    {
+        if (character == Character.Child)
+            searchkey = "Child";
+        else
+            searchkey = "Parent";
+    }
+}
---
+++ UnityProject/Assets/[5]_Scripts/[4]_GameScreen/CameraManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private Transform gamePosition;
+
+    public void GameCamera()
+    {
+        transform.position = gamePosition.position;
+        transform.rotation = gamePosition.rotation;
+    }
+}
