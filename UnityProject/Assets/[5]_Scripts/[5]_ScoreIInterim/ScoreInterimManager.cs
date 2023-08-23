743a573f3e1f06d1e678030906f18c548104cfac
Toffeefee8 <gibbon-preamp0x@icloud.com>
2023-08-23 03:45:45 +0200

    fixed endscreen

--- UnityProject/Assets/[5]_Scripts/[5]_ScoreIInterim/ScoreInterimManager.cs
+++ UnityProject/Assets/[5]_Scripts/[5]_ScoreIInterim/ScoreInterimManager.cs
@@ -2,23 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class BetweenRoundsManager : MonoBehaviour
 {
     [SerializeField] Button nextRoundButton;
+    [SerializeField] TextMeshPro parentText;
+    [SerializeField] TextMeshPro childText;
 
-    void Awake()
+    IEnumerator Start()
     {
-        GoToEndScreen();
+        PlayerData parent = GameData.GetData<PlayerData>("Parent");
+        PlayerData child = GameData.GetData<PlayerData>("Child");
+        PlayTimeData data = GameData.GetData<PlayTimeData>("PlayTimeData");
+
+
         if (nextRoundButton== null)
         {
             nextRoundButton = gameObject.AddComponent<Button>();
         }
         nextRoundButton.onClick.AddListener(() => ScreenSwitcher.SwitchScreen(ScreenType.GameScreen));
+
+        parentText.text = parent.tempScore.ToString();
+        childText.text = child.tempScore.ToString();
+
+        if (data.hasChildWon)
+            child.tempScore++;
+        else
+            parent.tempScore++;
+
+        yield return new WaitForSeconds(1);
+
+        parentText.text = parent.tempScore.ToString();
+        childText.text = child.tempScore.ToString();
+
+        yield return new WaitForSeconds(1);
+
+        if (child.tempScore >= 3 || parent.tempScore >= 3)
+        {
+            ScreenSwitcher.SwitchScreen(ScreenType.EndScreen);
+        }
+        else
+        {
+            ScreenSwitcher.SwitchScreen(ScreenType.GameScreen);
+        }
     }
 
-    void Update()
+    /*void Update()
     {
         WaitForKeyInput();
     }
@@ -38,5 +69,5 @@ public class BetweenRoundsManager : MonoBehaviour
             //Activate Endscreen
         }
         
-    }
+    }*/
 }
