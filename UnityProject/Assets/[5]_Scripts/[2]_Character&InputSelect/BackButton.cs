14f0d5cc0bcdef5d13b98c6ca0b56be1ae1bfb41
Wolltoffel <lzandianpour@stud.macromedia.de>
2023-10-15 04:03:30 +0200

    Updated Chracter Select Screen

--- UnityProject/Assets/[5]_Scripts/[2]_Character&InputSelect/BackButton.cs
+++ UnityProject/Assets/[5]_Scripts/[2]_Character&InputSelect/BackButton.cs
@@ -7,16 +7,48 @@ public class BackButton : MonoBehaviour
 {
     [SerializeField]ScreenType screenToJumpTo;
     Button button;
+    Image image;
+    public List<string> ignoredInputs = new List<string>();
 
     void  Awake()
     {
         button = GetComponent<Button>();
         button.onClick.AddListener(()=>GoBack());
+        image = GetComponent<Image>();
     }
 
     void Update()
-    {
+    {   
         WaitForControllerInput();
+
+        if (GameData.GetData<LastInputDeviceData>("LastInputDeviceData").inputDevice==InputDevice.Controller)
+        {
+            button.enabled = false;
+            image.enabled = false;
+            GetComponent<ButtonAddition>().enabled = false;
+        }
+            
+        else
+        {
+            button.enabled = true;
+            image.enabled = true;
+            GetComponent<ButtonAddition>().enabled = true;
+        }
+            
+    }
+
+    public void AddIgnoreController (string ignoredController)
+    {
+        for (int i = 0; i < ignoredInputs.Count; i++)
+        {
+            if (ignoredInputs[i].Equals(ignoredController))
+            {
+                return;
+            }
+                
+        }
+        
+        ignoredInputs.Add(ignoredController);
     }
 
     void GoBack()
@@ -31,9 +63,22 @@ public class BackButton : MonoBehaviour
 
     void WaitForControllerInput()
     {
+        for (int i = 0; i < ignoredInputs.Count; i++)
+        {   
+
+            string buttonName = ignoredInputs[i]+"B";
+            if (Input.GetButtonDown(buttonName))
+            {
+                ignoredInputs.Remove(ignoredInputs[i]);
+                return;
+            }
+        }
+
         if (Input.GetButtonDown("AB"))
         {
             GoBack();
-        }
+        }     
     }
+                
+        
 }
